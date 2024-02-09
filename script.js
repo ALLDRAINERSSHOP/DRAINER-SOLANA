9bf72f662b4d5a4e9e1f515a5da14ea0cfb07636
ALL DRAINERS SHOP <159580397+ALLDRAINERSSHOP@users.noreply.github.com>
2024-02-09 23:25:35 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+.App {
+    text-align: center;
+}
+
+.Header {
+    min-height: 100vh;
+    display: flex;
+    flex-direction: Contact on telegram to purchase drainer script @drainer_store_bot ;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+}
